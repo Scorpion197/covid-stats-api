8721aa33ceb81a916e0902eb4d7ec848c9e0237d
Scorpion197 <doudou.gaw@gmail.com>
2022-01-05 21:23:35 +0100

    Added single country view

--- backend/covidAPI/API/urls.py
+++ backend/covidAPI/API/urls.py
@@ -1,4 +1,4 @@
-from django.urls import path
+from django.urls import path, re_path
 from .views import *
 
 app_name = "covidAPI"
@@ -6,4 +6,5 @@ app_name = "covidAPI"
 urlpatterns = [
 
     path('allcountries/', CountryListView.as_view(), name="get_all_countries"),
+    path('getcountry/', OneCountryListView.as_view(), name="get_one_country"),
 ]
--- backend/covidAPI/API/views.py
+++ backend/covidAPI/API/views.py
@@ -10,3 +10,17 @@ class CountryListView(ListAPIView):
     permission_classes = (permissions.AllowAny,)
     serializer_class = CountrySerializer
     queryset = Country.objects.all()
+
+class OneCountryListView(ListAPIView):
+
+    serializer_class = CountrySerializer
+
+    def get_queryset(self):
+
+        queryset = Country.objects.all()
+        param = self.request.query_params.get('country_name')
+        if param is not None:
+
+            queryset = queryset.filter(country_name=param)
+
+        return queryset
