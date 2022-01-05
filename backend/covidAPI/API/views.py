d466038ef8527c57e6e06b80ef8885ccd2f981e4
Scorpion197 <doudou.gaw@gmail.com>
2022-01-05 20:38:06 +0100

    fixed some import errors in serializers and views

--- backend/covidAPI/API/views.py
+++ backend/covidAPI/API/views.py
@@ -2,7 +2,7 @@ from rest_framework.generics import ListAPIView, RetrieveAPIView
 from rest_framework import permissions
 
 from .serializers import CountrySerializer
-from ../models import Country
+from covidAPI.models import Country
 
 
 class CountryListView(ListAPIView):
@@ -10,5 +10,3 @@ class CountryListView(ListAPIView):
     permission_classes = (permissions.AllowAny,)
     serializer_class = CountrySerializer
     queryset = Country.objects.all()
-
-    
